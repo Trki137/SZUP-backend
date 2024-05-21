b2b73edf9e939bf717a55aafb089f265141a179e
Dean Trkulja <deantrkulja2001@gmail.com>
2024-05-21 10:22:19 +0200

    Init commit

---
+++ src/main/java/infsus/szup/SzupApplication.java
@@ -0,0 +1,13 @@
+package infsus.szup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzupApplication.class, args);
+	}
+
+}
