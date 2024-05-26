2cdc8e474abfc942a135d8a3d2cdeede1cd85060
jopica23 <126028293+jopica23@users.noreply.github.com>
2024-05-26 20:21:18 +0200

    date formating on TaskDetailsDTO

--- src/main/java/infsus/szup/model/dto/task/TaskDetailsDTO.java
+++ src/main/java/infsus/szup/model/dto/task/TaskDetailsDTO.java
@@ -1,13 +1,22 @@
 package infsus.szup.model.dto.task;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import infsus.szup.model.dto.priority.PriorityResponseDTO;
 import infsus.szup.model.dto.status.StatusResponseDTO;
 import infsus.szup.model.dto.users.UserResponseDTO;
 
 import java.time.LocalDateTime;
 
-public record TaskDetailsDTO(Long id, String taskName, Long solverTeamId, LocalDateTime taskSetDate,
-                             LocalDateTime taskEndDate,
-                             String description, UserResponseDTO taskOwner, UserResponseDTO taskSolver,
-                             StatusResponseDTO currentStatus, PriorityResponseDTO priority) {
+public record TaskDetailsDTO(
+    Long id,
+    String taskName,
+    Long solverTeamId,
+    @JsonProperty("taskSetDate") @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime taskSetDate,
+    @JsonProperty("taskEndDate") @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime taskEndDate,
+    String description,
+    UserResponseDTO taskOwner,
+    UserResponseDTO taskSolver,
+    StatusResponseDTO currentStatus,
+    PriorityResponseDTO priority) {
 }
